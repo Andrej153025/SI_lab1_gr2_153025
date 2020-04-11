4687334b260eb56201ef0ca59456a09a41dd62b4
Andrej153025 <63502690+Andrej153025@users.noreply.github.com>
2020-04-11 19:44:44 +0200

    Grades list added

--- Student.java
+++ Student.java
@@ -2,10 +2,48 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
+	int[] Grades;
 
-	//TODO constructor
+	public Student(String index, String firstName, String lastName, int[] labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.Grades = Grades;
 
-	//TODO seters & getters
+		public String getIndex() {
+			return index;
+		}
+
+		public void setIndex(String index) {
+			this.index = index;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public int[] getGrades() {
+			return Grades;
+		}
+
+		public void setGrades(int[] grades) {
+			Grades = grades;
+		}
+
+
+
 
 	public double getAverage() {
 		//TODO
@@ -15,3 +53,15 @@ class Student {
 		//TODO
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
